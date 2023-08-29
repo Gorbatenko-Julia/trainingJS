5ce4dbbb6c51d2d580ce85cde519ae75d07aaeac
Julia Gorbatenko <ellis.tom@internet.ru>
2023-08-29 17:49:59 +0300

    added a comment

--- tasks/cycle/cycle1.js
+++ tasks/cycle/cycle1.js
@@ -1,3 +1,34 @@
+
+
+Даны числа a и b. Выведите строку с числами между а и b включая границы, отсортированными по возрастанию. Неизвестно, какое из чисел больше, но известно, что и a и b больше 0.
+
+Sample Input 1:
+
+8 1
+
+Sample Output 1:
+
+1 2 3 4 5 6 7 8
+
+Sample Input 2:
+
+6 6
+
+Sample Output 2:
+
+6
+
+Sample Input 3:
+
+3 5
+
+Sample Output 3:
+
+3 4 5
+
+
+
+
 function testCycle(a, b) {
     let x = "";
     
